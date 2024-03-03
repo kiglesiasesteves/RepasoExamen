51a37f5015f2f9cce6ab4069c7a39433f1387b34
Keyla Iglesias <kiglesiasesteves@danielcastelao.org>
2024-03-03 22:06:05 +0100

    Intercambiar

--- src/Intercambiar.java
+++ src/Intercambiar.java
@@ -6,7 +6,7 @@ public class Intercambiar {
                 CuandoCondicionesCumplidas(array, i, j);
             } else {
                 System.out.println("Índices fuera de rango");
-                ImprimirArray(array);
+                ImprimirArray(array);//índices
             }
         } else{
             System.out.println("Los índices corresponde al mismo valor.");
